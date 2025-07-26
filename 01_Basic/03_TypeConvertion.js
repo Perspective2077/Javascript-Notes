1ad8c90d9675cbc9a7644dadaf990994b9386d67
Perspective <115461545+Perspective2077@users.noreply.github.com>
2025-07-26 19:03:14 +0530

    Update 03_TypeConvertion.js

--- 01_Basic/03_TypeConvertion.js
+++ 01_Basic/03_TypeConvertion.js
@@ -1,10 +1,9 @@
 // Type Convertion :
 
-
 // Converting Into Numbers
 // To Covert Any value to number u can use "Number()" fnc 
 let string = "String"   
-let Converted = Number(string)        //String => Number , non-numeric string will be converted to "NaN"(i.e. Not a Number , it is still a number datatype) value and if the string is empty (i.e. "") ,it will return 0
+let Converted = Number(string)        //String => Number , non-numeric string will be converted to "NaN"( Not a Number , NaN is still a number datatype) and if the string is empty (i.e. "") ,it will return 0
 
 let boolean = true  
 let Converted1 = Number(boolean)     //Boolean => Number lead to Either 1 for true or 0 for false 
@@ -26,7 +25,7 @@ let converting_into_Number1 = +true
 // Converting Into String
 // To Covert Any value to String u can use "String()" or ".tostring()" fnc  
 let number = 69  
-let Converted4 = String(number)       //String => String lead to "NaN" i.e. Not a String value and if the string is empty i.e. "" , it will return 0
+let Converted4 = String(number)       // number => string, will convert number into string datatype. including bigInt,float,NaN .. DUH
 
 //Boolean,null,Undefined => String ( All will convert their value to string i.e. "Undefined","null","true/false")
 
@@ -44,4 +43,4 @@ let Converted6 = String(object)       //Objects => String will give u array insi
 // To Convert Any value to Boolean, you can use the "Boolean()" function 
 let number1 = 0
 let Converted7 = Boolean(number1)       //Number => Boolean, 0 will be false, any other number will be true same goes for string 
-// empty string("") ,0,Bigint 0n,undefined,null are falsy(false) value when converted into bool while empty array,object,function are truthy(true) value
+// empty string("") ,0,Bigint 0n,NaN,undefined,null are falsy(false) value when converted into bool while empty array,object,function are truthy(true) value
